2136ab92cefa3e691dcbeeddeffcec9d38924653
SenatorTrent <strentmccauley@gmail.com>
2025-07-06 16:51:07 -0700

    Complete Ex06 & Ex07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, x) {
+    if(x>0){
+        let repeat = string;
+        while (x-1>0){
+            string += repeat;
+            x--;
+        }
+    return string;
+    }
+    else if (x<0){
+        return "ERROR";
+    }
+    return "";
 };
 
 // Do not edit below this line
